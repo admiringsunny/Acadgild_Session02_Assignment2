a81e8eeb791c833a179a6d197a11dd159e8ff691
admiringsunny <admiringsunny@gmail.com>
2016-07-23 22:39:50 +0530

    Session02 Assignment2

---
+++ s2_a2_vibgyor_screen/app/src/main/java/com/acadgild/s2A2VIBGYORScreen/VIBGYOR.java
@@ -0,0 +1,13 @@
+package com.acadgild.s2A2VIBGYORScreen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VIBGYOR extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vibgyor);
+    }
+}
